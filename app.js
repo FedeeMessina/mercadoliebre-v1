9b5481f3bf855abfbe45e823e68ec291cdca20f6
FedeeMessina <federico.messina@hotmail.com>
2021-09-16 22:34:21 -0300

    Primer repo de Mercado Liebre

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = process.env.port || 5000;
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen(port, () =>
+  console.log("El servidor esta andando perfectamente http://localhost:5000")
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
